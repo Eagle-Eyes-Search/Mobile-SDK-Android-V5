8642d0edbd1a65a9863fa36b38eb019bde826e91
peter <peter.ed.oconnor@gmail.com>
2024-09-26 21:15:40 -0700

    refectored by gpt

--- SampleCode-V5/android-sdk-v5-sample/src/main/java/dji/simpleV5/DJIMainActivity.kt
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/dji/simpleV5/DJIMainActivity.kt
@@ -1,7 +1,6 @@
 package dji.simpleV5
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
@@ -12,11 +11,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import dji.sampleV5.aircraft.R
-import dji.simpleV5.utils.BaseMainActivityVm
-import dji.simpleV5.utils.MSDKInfoVm
-import dji.simpleV5.utils.MSDKManagerVM
-import dji.simpleV5.utils.ToastUtils
-import dji.simpleV5.utils.globalViewModels
+import dji.simpleV5.utils.*
 import dji.v5.common.utils.GeoidManager
 import dji.v5.utils.common.LogUtils
 import dji.v5.utils.common.PermissionUtil
@@ -25,89 +20,29 @@ import dji.v5.ux.core.communication.DefaultGlobalPreferences
 import dji.v5.ux.core.communication.GlobalPreferencesManager
 import dji.v5.ux.core.util.UxSharedPreferencesUtil
 import io.reactivex.rxjava3.disposables.CompositeDisposable
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import kotlinx.android.synthetic.main.activity_main.*
 
-/**
- * Class Description
- *
- * @author Hoker
- * @date 2022/2/10
- *
- * Copyright (c) 2022, DJI All Rights Reserved.
- */
 class DJIMainActivity : AppCompatActivity() {
 
-    val tag: String = LogUtils.getTag(this)
-    private val permissionArray = arrayListOf(
-        Manifest.permission.RECORD_AUDIO,
-        Manifest.permission.KILL_BACKGROUND_PROCESSES,
-        Manifest.permission.ACCESS_COARSE_LOCATION,
-        Manifest.permission.ACCESS_FINE_LOCATION,
-    )
-
-    init {
-        permissionArray.apply {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                add(Manifest.permission.READ_MEDIA_IMAGES)
-                add(Manifest.permission.READ_MEDIA_VIDEO)
-                add(Manifest.permission.READ_MEDIA_AUDIO)
-            } else {
-                add(Manifest.permission.READ_EXTERNAL_STORAGE)
-                add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            }
-        }
-    }
-
+    private val tag: String = LogUtils.getTag(this)
     private val baseMainActivityVm: BaseMainActivityVm by viewModels()
     private val msdkInfoVm: MSDKInfoVm by viewModels()
     private val msdkManagerVM: MSDKManagerVM by globalViewModels()
     private val handler: Handler = Handler(Looper.getMainLooper())
     private val disposable = CompositeDisposable()
-//    private val primaryFpvWidget = findViewById<FPVWidget?>(R.id.widget_primary_fpv)
 
-    fun prepareUxActivity() {
-        UxSharedPreferencesUtil.initialize(this)
-        GlobalPreferencesManager.initialize(DefaultGlobalPreferences(this))
-        GeoidManager.getInstance().init(this)
-
-        enableDefaultLayout(SimplePilotingActivity::class.java)
-
-//        primaryFpvWidget.updateVideoSource(ComponentIndexType.LEFT_OR_MAIN)
-
-    }
-
-    fun prepareTestingToolsActivity() {}
+    private val permissionArray = getPermissionArray()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 有一些手机从系统桌面进入的时候可能会重启main类型的activity
-        // 需要校验这种情况，业界标准做法，基本所有app都需要这个
-        if (!isTaskRoot && intent.hasCategory(Intent.CATEGORY_LAUNCHER) && Intent.ACTION_MAIN == intent.action) {
+        if (shouldFinishActivity()) return
 
-                finish()
-                return
-
-        }
-
-        window.decorView.apply {
-            systemUiVisibility =
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        }
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 
         initMSDKInfoView()
         observeSDKManager()
@@ -116,137 +51,12 @@ class DJIMainActivity : AppCompatActivity() {
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (checkPermission()) {
-            handleAfterPermissionPermitted()
-        }
+        if (checkPermission()) handleAfterPermissionPermitted()
     }
 
     override fun onResume() {
         super.onResume()
-        if (checkPermission()) {
-            handleAfterPermissionPermitted()
-        }
-    }
-
-    private fun handleAfterPermissionPermitted() {
-        prepareTestingToolsActivity()
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun initMSDKInfoView() {
-        msdkInfoVm.msdkInfo.observe(this) {
-            text_view_version.text = StringUtils.getResStr(R.string.sdk_version, it.SDKVersion + " " + it.buildVer)
-            text_view_product_name.text = StringUtils.getResStr(R.string.product_name, it.productType.name)
-            text_view_package_product_category.text = StringUtils.getResStr(R.string.package_product_category, it.packageProductCategory)
-            text_view_is_debug.text = StringUtils.getResStr(R.string.is_sdk_debug, it.isDebug)
-//            text_core_info.text = it.coreInfo.toString()
-        }
-
-//        icon_sdk_forum.setOnClickListener {
-//            Helper.startBrowser(this, StringUtils.getResStr(R.string.sdk_forum_url))
-//        }
-//        icon_release_node.setOnClickListener {
-//            Helper.startBrowser(this, StringUtils.getResStr(R.string.release_node_url))
-//        }
-//        icon_tech_support.setOnClickListener {
-//            Helper.startBrowser(this, StringUtils.getResStr(R.string.tech_support_url))
-//        }
-        view_base_info.setOnClickListener {
-            baseMainActivityVm.doPairing {
-                showToast(it)
-            }
-        }
-    }
-
-    private fun observeSDKManager() {
-        msdkManagerVM.lvRegisterState.observe(this) { resultPair ->
-            val statusText: String?
-            if (resultPair.first) {
-                ToastUtils.showToast("Register Success")
-                statusText = StringUtils.getResStr(this, R.string.registered)
-                msdkInfoVm.initListener()
-                handler.postDelayed({
-                    prepareUxActivity()
-                }, 5000)
-            } else {
-                showToast("Register Failure: ${resultPair.second}")
-                statusText = StringUtils.getResStr(this, R.string.unregistered)
-            }
-            text_view_registered.text = StringUtils.getResStr(R.string.registration_status, statusText)
-        }
-
-        msdkManagerVM.lvProductConnectionState.observe(this) { resultPair ->
-            showToast("Product: ${resultPair.second} ,ConnectionState:  ${resultPair.first}")
-        }
-
-        msdkManagerVM.lvProductChanges.observe(this) { productId ->
-            showToast("Product: $productId Changed")
-        }
-
-        msdkManagerVM.lvInitProcess.observe(this) { processPair ->
-            showToast("Init Process event: ${processPair.first.name}")
-        }
-
-        msdkManagerVM.lvDBDownloadProgress.observe(this) { resultPair ->
-            showToast("Database Download Progress current: ${resultPair.first}, total: ${resultPair.second}")
-        }
-    }
-
-    private fun showToast(content: String) {
-        ToastUtils.showToast(content)
-
-    }
-
-
-    fun <T> enableDefaultLayout(cl: Class<T>) {
-        enableShowCaseButton(default_layout_button, cl)
-    }
-
-//    fun <T> enableWidgetList(cl: Class<T>) {
-//        enableShowCaseButton(widget_list_button, cl)
-//    }
-//
-//    fun <T> enableTestingTools(cl: Class<T>) {
-//        enableShowCaseButton(testing_tool_button, cl)
-//    }
-
-    private fun <T> enableShowCaseButton(view: View, cl: Class<T>) {
-        view.isEnabled = true
-        view.setOnClickListener {
-            Intent(this, cl).also {
-                startActivity(it)
-            }
-        }
-    }
-
-    private fun checkPermissionAndRequest() {
-        if (!checkPermission()) {
-            requestPermission()
-        }
-    }
-
-    private fun checkPermission(): Boolean {
-        for (i in permissionArray.indices) {
-            if (!PermissionUtil.isPermissionGranted(this, permissionArray[i])) {
-                return false
-            }
-        }
-        return true
-    }
-
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) { result ->
-        result?.entries?.forEach {
-            if (!it.value) {
-                requestPermission()
-                return@forEach
-            }
-        }
-    }
-
-    private fun requestPermission() {
-        requestPermissionLauncher.launch(permissionArray.toArray(arrayOf()))
+        if (checkPermission()) handleAfterPermissionPermitted()
     }
 
     override fun onDestroy() {
@@ -254,79 +64,122 @@ class DJIMainActivity : AppCompatActivity() {
         handler.removeCallbacksAndMessages(null)
         disposable.dispose()
     }
-}
 
+    private fun shouldFinishActivity(): Boolean {
+        return if (!isTaskRoot && intent.hasCategory(Intent.CATEGORY_LAUNCHER) && Intent.ACTION_MAIN == intent.action) {
+            finish()
+            true
+        } else false
+    }
 
-@Composable
-fun MainContent() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Black)
-    ) {
-        // Info Panel Section
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.LightGray)
-                .padding(16.dp)
-        ) {
-            Text(
-                text = "SDK Version", // Replace with actual SDK version
-                color = Color.White,
-                fontSize = 14.sp,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
-            Text(
-                text = "Package Product Category",
-                color = Color.White,
-                fontSize = 14.sp,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
-            Text(
-                text = "Unregistered",
-                color = Color.White,
-                fontSize = 14.sp,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
-            Text(
-                text = "Product Name",
-                color = Color.White,
-                fontSize = 14.sp,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
-            Text(
-                text = "Is SDK Debug",
-                color = Color.White,
-                fontSize = 14.sp,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
+    private fun handleAfterPermissionPermitted() {
+        prepareTestingToolsActivity()
+    }
+
+    private fun initMSDKInfoView() {
+        msdkInfoVm.msdkInfo.observe(this) {
+            text_view_version.text = StringUtils.getResStr(R.string.sdk_version, "${it.SDKVersion} ${it.buildVer}")
+            text_view_product_name.text = StringUtils.getResStr(R.string.product_name, it.productType.name)
+            text_view_package_product_category.text = StringUtils.getResStr(R.string.package_product_category, it.packageProductCategory)
+            text_view_is_debug.text = StringUtils.getResStr(R.string.is_sdk_debug, it.isDebug)
         }
 
-        // Scrollable button section (Case Panel)
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(top = 2.dp)
-                .verticalScroll(rememberScrollState())
-                .background(Color.LightGray)
-                .padding(8.dp)
-        ) {
-            Button(
-                onClick = { /* Handle Enter Cockpit click */ },
-                enabled = false,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-            ) {
-                Text(text = "Enter Cockpit")
+        view_base_info.setOnClickListener {
+            baseMainActivityVm.doPairing { showToast(it) }
+        }
+    }
+
+    private fun observeSDKManager() {
+        msdkManagerVM.lvRegisterState.observe(this) { resultPair ->
+            val statusText = if (resultPair.first) {
+                showToast("Register Success")
+                StringUtils.getResStr(this, R.string.registered).also { msdkInfoVm.initListener() }
+            } else {
+                showToast("Register Failure: ${resultPair.second}")
+                StringUtils.getResStr(this, R.string.unregistered)
+            }
+            text_view_registered.text = StringUtils.getResStr(R.string.registration_status, statusText)
+            if (resultPair.first) handler.postDelayed({ prepareUxActivity() }, 5000)
+        }
+
+        msdkManagerVM.lvProductConnectionState.observe(this) {
+            showToast("Product: ${it.second}, ConnectionState: ${it.first}")
+        }
+
+        msdkManagerVM.lvProductChanges.observe(this) {
+            showToast("Product: $it Changed")
+        }
+
+        msdkManagerVM.lvInitProcess.observe(this) {
+            showToast("Init Process event: ${it.first.name}")
+        }
+
+        msdkManagerVM.lvDBDownloadProgress.observe(this) {
+            showToast("Database Download Progress: ${it.first}/${it.second}")
+        }
+    }
+
+    private fun showToast(content: String) {
+        ToastUtils.showToast(content)
+    }
+
+    private fun <T> enableDefaultLayout(cl: Class<T>) {
+        enableShowCaseButton(default_layout_button, cl)
+    }
+
+    private fun <T> enableShowCaseButton(view: View, cl: Class<T>) {
+        view.isEnabled = true
+        view.setOnClickListener {
+            Intent(this, cl).also { startActivity(it) }
+        }
+    }
+
+    private fun checkPermissionAndRequest() {
+        if (!checkPermission()) requestPermission()
+    }
+
+    private fun checkPermission(): Boolean {
+        return permissionArray.all { PermissionUtil.isPermissionGranted(this, it) }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { result ->
+        result.entries.forEach { if (!it.value) requestPermission() }
+    }
+
+    private fun requestPermission() {
+        requestPermissionLauncher.launch(permissionArray.toArray(arrayOf()))
+    }
+
+    private fun prepareUxActivity() {
+        UxSharedPreferencesUtil.initialize(this)
+        GlobalPreferencesManager.initialize(DefaultGlobalPreferences(this))
+        GeoidManager.getInstance().init(this)
+        enableDefaultLayout(SimplePilotingActivity::class.java)
+    }
+
+    private fun prepareTestingToolsActivity() {}
+
+    private fun getPermissionArray(): ArrayList<String> {
+        return arrayListOf(
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.KILL_BACKGROUND_PROCESSES,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+        ).apply {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                addAll(listOf(
+                    Manifest.permission.READ_MEDIA_IMAGES,
+                    Manifest.permission.READ_MEDIA_VIDEO,
+                    Manifest.permission.READ_MEDIA_AUDIO
+                ))
+            } else {
+                addAll(listOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ))
             }
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewMainContent() {
-    MainContent()
 }
